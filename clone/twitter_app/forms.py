62ca8b981775d3ad769d1597c03b07007bbd699a
kaif <Mohamadkaif@nividata.com>
2023-03-07 11:12:44 +0530

    edit profile

---
+++ clone/twitter_app/forms.py
@@ -0,0 +1,14 @@
+
+from pyexpat import model
+
+from django.forms import ModelForm, fields, widgets
+from django.contrib.auth.models import User
+class user_model(ModelForm):
+    class Meta:
+        model=User
+        fields=['username','email','password']
+
+# class tweet_modelform(ModelForm):
+#     class Meta:
+#         model=Tuser_tweets
+#         fields='__all__'
